7aa4438a7bc8f73fbf846b61bd2b41ea67cfb609
Airat Kayumov <st128100@student.spbu.ru>
2025-12-23 16:19:49 +0300

    Added scripts for future notebooks optimization.

--- rs/model.py
+++ rs/model.py
@@ -1,35 +1,49 @@
-import torch
 import torch.nn as nn
 from torchvision.ops import sigmoid_focal_loss
+
+class BasePredictor(nn.Module):
+    def __init__(self, input_size=511, hidden_size=512, output_size=255):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+
+            nn.Linear(hidden_size, output_size)
+        )
+    
+    def forward(self, x):
+        return self.net(x)
 
 class PositionPredictor(nn.Module):
     def __init__(self, input_size=511, hidden_size=512, output_size=255, dropout=0.1):
         super().__init__()
         
         layers = []
+        in_features = input_size
         
-        layers.append(nn.Linear(input_size, hidden_size))
-        layers.append(nn.BatchNorm1d(hidden_size))
-        layers.append(nn.ReLU())
-        layers.append(nn.Dropout(dropout))
-        
-        for _ in range(3):
-            layers.append(nn.Linear(hidden_size, hidden_size))
-            layers.append(nn.BatchNorm1d(hidden_size))
-            layers.append(nn.ReLU())
-            layers.append(nn.Dropout(dropout))
+        for _ in range(4):
+            layers.extend([
+                nn.Linear(in_features, hidden_size),
+                nn.BatchNorm1d(hidden_size),
+                nn.ReLU(),
+                nn.Dropout(dropout),
+            ])
+            in_features = hidden_size
         
         layers.append(nn.Linear(hidden_size, output_size))
-        
         self.net = nn.Sequential(*layers)
     
     def forward(self, x):
         return self.net(x)
-    
-    def predict_proba(self, x):
-        with torch.no_grad():
-            logits = self.forward(x)
-            return torch.sigmoid(logits)
 
 class FocalLoss(nn.Module):
     def __init__(self, alpha=0.2, gamma=1.5):
